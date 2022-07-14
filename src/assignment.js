18ed5f9ad1f81d21b6a6f2c0bd1db35a14c96508
Amarachi onyekwere <99353844+amaonyekwere2022@users.noreply.github.com>
2022-07-14 18:54:06 +0000

    src/assignment

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0
+  for(i= 0; i< arrayOfNumbers.lenght; i++){
+      sum += arrayOfNumbers[i]
+  }
+    return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -28,9 +33,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0
+  for(i=0; i < arrayOfNumbers.lenght; i++){
+      if(arrayOfNumbers[i] % 2 ===0){
+        
+      }
+  }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers =
+  countEvenNumbers;
+
 
 /**
  * Challenge - 3
